1aa78ae8cdb7b6838ef11d8705d561c14e3bb29d
Ivan Komissarov <abbapoh@gmail.com>
2018-05-08 20:28:19 +0300

    A bit more tests

--- tests/auto/test_texture/test_texture.cpp
+++ tests/auto/test_texture/test_texture.cpp
@@ -13,12 +13,22 @@ void TestTexture::defaultConstructed()
     Texture tex;
     QCOMPARE(tex.isNull(), true);
     QCOMPARE(tex.type(), Texture::Type::None);
+    QCOMPARE(tex.format(), Texture::Format::Invalid);
     QCOMPARE(tex.width(), 0);
     QCOMPARE(tex.height(), 0);
     QCOMPARE(tex.depth(), 0);
     QCOMPARE(tex.levels(), 0);
     QCOMPARE(tex.layers(), 0);
-    QCOMPARE(tex.format(), Texture::Format::Invalid);
+
+    QCOMPARE(tex.bytesPerTexel(), 0);
+    QCOMPARE(tex.bytesPerLine(), 0);
+    QCOMPARE(tex.bytesPerImage(), 0);
+
+    QCOMPARE(tex.data(), nullptr);
+    QCOMPARE(tex.constData(), nullptr);
+
+    QCOMPARE(tex.scanLine(0), nullptr);
+    QCOMPARE(tex.constScanLine(0), nullptr);
 }
 
 QTEST_MAIN(TestTexture)
