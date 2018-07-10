aff5500cf50ec509c9a8feaf50f784033e9f73f8
Ivan Komissarov <ABBAPOH@gmail.com>
2018-07-10 19:41:17 +0200

    Add OGL types to the TexelFormat struct

--- src/libs/texturelib/texelformat.cpp
+++ src/libs/texturelib/texelformat.cpp
@@ -2,12 +2,12 @@
 
 static const TexelFormat formats[] = {
     {},
-    {Texture::Format::RGBA_8888, 32, 0},
-    {Texture::Format::BGRA_8888, 32, 0},
-    {Texture::Format::RGB_888, 24, 0},
-    {Texture::Format::DXT1, 0, 8},
-    {Texture::Format::DXT3, 0, 16},
-    {Texture::Format::DXT5, 0, 16},
+    {Texture::Format::RGBA_8888, 32, 0, QOpenGLTexture::RGBA8_UNorm, QOpenGLTexture::RGBA, QOpenGLTexture::UInt8},
+    {Texture::Format::BGRA_8888, 32, 0, QOpenGLTexture::RGBA8_UNorm, QOpenGLTexture::BGRA, QOpenGLTexture::UInt8},
+    {Texture::Format::RGB_888, 24, 0, QOpenGLTexture::RGB8_UNorm, QOpenGLTexture::RGB, QOpenGLTexture::UInt8},
+    {Texture::Format::DXT1, 0, 8, QOpenGLTexture::QOpenGLTexture::RGBA_DXT1},
+    {Texture::Format::DXT3, 0, 16, QOpenGLTexture::QOpenGLTexture::RGBA_DXT1},
+    {Texture::Format::DXT5, 0, 16, QOpenGLTexture::QOpenGLTexture::RGBA_DXT1},
 };
 
 static_assert (sizeof (formats) == sizeof(TexelFormat) * size_t(Texture::Format::FormatsCount),
--- src/libs/texturelib/texelformat.h
+++ src/libs/texturelib/texelformat.h
@@ -3,6 +3,8 @@
 
 #include <TextureLib/Texture>
 
+#include <QtGui/QOpenGLTexture>
+
 class TexelFormat
 {
 public:
@@ -10,15 +12,33 @@ public:
     constexpr inline TexelFormat(
             Texture::Format format,
             int bitsPerTexel,
-            int blockSize)
+            int blockSize,
+            QOpenGLTexture::TextureFormat oglTextureFormat,
+            QOpenGLTexture::PixelFormat oglPixelFormat = QOpenGLTexture::PixelFormat::NoSourceFormat,
+            QOpenGLTexture::PixelType oglPixelType = QOpenGLTexture::PixelType::NoPixelType)
         : m_format(format)
         , m_bitsPerTexel(bitsPerTexel)
         , m_blockSize(blockSize)
+        , m_oglTextureFormat(oglTextureFormat)
+        , m_oglPixelFormat(oglPixelFormat)
+        , m_oglPixelType(oglPixelType)
     {}
 
     constexpr inline Texture::Format format() const { return m_format; }
     constexpr inline int bitsPerTexel() const { return m_bitsPerTexel; }
     constexpr inline int blockSize() const { return m_blockSize; }
+
+    constexpr inline QOpenGLTexture::TextureFormat oglTextureFormat() const {
+        return m_oglTextureFormat;
+    }
+
+    constexpr inline QOpenGLTexture::PixelFormat oglPixelFormat() const {
+        return m_oglPixelFormat;
+    }
+
+    constexpr inline QOpenGLTexture::PixelType oglPixelType() const {
+        return m_oglPixelType;
+    }
 
     static const TexelFormat &texelFormat(Texture::Format format);
 
@@ -26,6 +46,9 @@ private:
     Texture::Format m_format {Texture::Format::Invalid};
     int m_bitsPerTexel {0};
     int m_blockSize {0};
+    QOpenGLTexture::TextureFormat m_oglTextureFormat {QOpenGLTexture::TextureFormat::NoFormat};
+    QOpenGLTexture::PixelFormat m_oglPixelFormat {QOpenGLTexture::PixelFormat::NoSourceFormat};
+    QOpenGLTexture::PixelType m_oglPixelType {QOpenGLTexture::PixelType::NoPixelType};
 };
 
 #endif // TEXELFORMAT_H
