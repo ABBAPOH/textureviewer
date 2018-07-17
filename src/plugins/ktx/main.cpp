17a639590e925c65d0023f7e289ceeaf7eed840a
Ivan Komissarov <ABBAPOH@gmail.com>
2018-07-17 22:21:34 +0300

    Add empty ktx plugin

---
+++ src/plugins/ktx/main.cpp
@@ -0,0 +1,29 @@
+//#include "ktxhandler.h"
+
+#include <TextureLib/TextureIOHandler>
+
+class KtxHandlerPlugin : public TextureIOHandlerPlugin
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(KtxHandlerPlugin)
+    Q_PLUGIN_METADATA(IID "org.arch.ImageDocument.DdsHandlerPlugin" FILE "ktx.json")
+
+public:
+    KtxHandlerPlugin() = default;
+
+    std::unique_ptr<TextureIOHandler> create(const QMimeType &mimeType) override
+    {
+//        if (mimeType.name() == u"image/x-ktx")
+//            return std::make_unique<KtxHandler>();
+        return nullptr;
+    }
+
+    Capabilities capabilities(const QMimeType &mimeType) const override
+    {
+        if (mimeType.name() == u"image/x-dds")
+            return Capabilities(KtxHandlerPlugin::CanRead);
+        return Capabilities();
+    }
+};
+
+#include "main.moc"
--- src/plugins/plugins.qbs
+++ src/plugins/plugins.qbs
@@ -3,6 +3,7 @@ import qbs.base 1.0
 Project {
     references: [
         "dds/dds.qbs",
+        "ktx/ktx.qbs",
         "pkm/pkm.qbs",
         "vtf/vtf.qbs",
     ]
