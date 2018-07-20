16d13aa73ea85300603017bd9455d9b92f83e694
Ivan Komissarov <ABBAPOH@gmail.com>
2018-07-20 20:01:38 +0300

    Add options

--- tests/manual/bind_texture/main.cpp
+++ tests/manual/bind_texture/main.cpp
@@ -1,8 +1,38 @@
 #include <QGuiApplication>
 #include <QTimer>
+#include <QCommandLineParser>
 #include <TextureLib/TextureIO>
 
 #include "window.h"
+
+struct Options
+{
+    bool coreProfile {false};
+    QString inputFile;
+};
+
+Options parseOptions(const QStringList &arguments)
+{
+    QCommandLineParser parser;
+    QCommandLineOption coreProfileOption(QStringLiteral("core-profile"),
+                                         QStringLiteral("Use OpenGL 3.3 instead of OpenGL ES"));
+    parser.addOption(coreProfileOption);
+    parser.addPositionalArgument(QStringLiteral("input"),
+                                 QStringLiteral("Input filename"),
+                                 QStringLiteral("input"));
+
+    parser.process(arguments);
+
+    const auto positional = parser.positionalArguments();
+    if (positional.size() != 1) {
+        parser.showHelp(1);
+    }
+
+    Options options;
+    options.coreProfile = parser.isSet(coreProfileOption);
+    options.inputFile = positional.at(0);
+    return options;
+}
 
 int main(int argc, char *argv[])
 {
@@ -11,14 +41,13 @@ int main(int argc, char *argv[])
 
     QGuiApplication a(argc, argv);
 
-    if (argc != 2) {
-        qInfo() << "Usage: ./bind_texture [texture]";
-        return 1;
-    }
+    QCoreApplication::setApplicationName("bind_texture");
+
+    const auto options = parseOptions(QCoreApplication::arguments());
 
     a.addLibraryPath(a.applicationDirPath() + TextureIO::pluginsDirPath());
 
-    TextureIO io(QString::fromLocal8Bit(argv[1]));
+    TextureIO io(options.inputFile);
     auto ok = io.read();
     if (!ok) {
         qCritical() << "Can't load texture" << toUserString(ok.error());
@@ -26,8 +55,10 @@ int main(int argc, char *argv[])
     }
 
     QSurfaceFormat fmt;
-    fmt.setVersion(3, 3);
-    fmt.setProfile(QSurfaceFormat::CoreProfile);
+    if (options.coreProfile) {
+        fmt.setVersion(3, 3);
+        fmt.setProfile(QSurfaceFormat::CoreProfile);
+    }
     QSurfaceFormat::setDefaultFormat(fmt);
 
     Window w(*ok);
