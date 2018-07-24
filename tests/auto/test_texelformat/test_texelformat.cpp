fd12a94de26af18ef6dd981527a0b983f6e8ac42
Ivan Komissarov <ABBAPOH@gmail.com>
2018-07-24 15:50:53 +0300

    Add findOGLFormat test

--- tests/auto/test_texelformat/test_texelformat.cpp
+++ tests/auto/test_texelformat/test_texelformat.cpp
@@ -6,11 +6,24 @@ class TestTexelFormat : public QObject
 {
     Q_OBJECT
 private slots:
+    void testFindOGLFormat();
 #if defined(Q_OS_LINUX)
     void benchFindOGLFormatConst();
 #endif // Q_OS_LINUX
     void benchFindOGLFormatLinear();
 };
+
+void TestTexelFormat::testFindOGLFormat()
+{
+    const auto formats = TexelFormat::texelFormats();
+    for (const auto &i: formats) {
+        if (i.format() == Texture::Format::RXGB)
+            continue; // Not supported in ktx
+        auto j = TexelFormat::findOGLFormatLinear(i.oglTextureFormat(), i.oglPixelFormat(), i.oglPixelType());
+        if (i != j)
+            QCOMPARE(j, i);
+    }
+}
 
 #if defined(Q_OS_LINUX)
 void TestTexelFormat::benchFindOGLFormatConst()
