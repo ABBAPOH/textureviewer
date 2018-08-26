29b41b4a09439bdb9c720ee56e6cfd2936b51ede
Ivan Komissarov <ABBAPOH@gmail.com>
2018-08-26 17:45:58 +0300

    Add convertion functions for Rgba32Signed

--- src/libs/texturelib/rgba32signed.h
+++ src/libs/texturelib/rgba32signed.h
@@ -27,7 +27,7 @@ class Rgba32Signed
 public:
     constexpr Rgba32Signed() noexcept = default;
     explicit constexpr Rgba32Signed(quint32 c) noexcept : m_rgba(c) {}
-    explicit constexpr Rgba32Signed(qint8 red, qint8 green, qint8 blue, qint8 alpha = 127) noexcept
+    constexpr Rgba32Signed(qint8 red, qint8 green, qint8 blue, qint8 alpha = 127) noexcept
     {
         m_rgba = quint32(quint8(red))   << RedShift
                | quint32(quint8(green)) << GreenShift
@@ -67,5 +67,55 @@ public:
         m_rgba = (m_rgba & ~(0xffu << AlphaShift)) | (quint32(quint8(alpha)) << AlphaShift);
     }
 };
+
+// TODO (abbapoh) : we can't enable this overload because QRgb is quint32 too (=
+// I hope this will be fixed in Qt6
+//inline constexpr Rgba32Signed rgba32Signed(quint32 value) noexcept
+//{
+//    return Rgba32Signed(value);
+//}
+
+inline constexpr Rgba32Signed rgba32Signed(qint8 r, qint8 g, qint8 b, qint8 a = 127) noexcept
+{
+    return {r, g, b, a};
+}
+
+// QRgb convertions
+
+inline constexpr Rgba32Signed rgba32Signed(QRgb rgba) noexcept
+{
+    return {qint8(qRed(rgba)   >> 1u),
+            qint8(qGreen(rgba) >> 1u),
+            qint8(qBlue(rgba)  >> 1u),
+            qint8(qAlpha(rgba) >> 1u)};
+}
+
+inline constexpr QRgb qRgba(Rgba32Signed rgba)
+{
+    return qRgba(
+            quint8(std::max<qint8>(0, rgba.red()))   << 1u,
+            quint8(std::max<qint8>(0, rgba.green())) << 1u,
+            quint8(std::max<qint8>(0, rgba.blue()))  << 1u,
+            quint8(std::max<qint8>(0, rgba.alpha())) << 1u);
+}
+
+// QRgba64 convertions
+
+inline constexpr Rgba32Signed rgba32Signed(QRgba64 rgba) noexcept
+{
+    return {qint8(qRed(rgba)   >> 9u),
+            qint8(qGreen(rgba) >> 9u),
+            qint8(qBlue(rgba)  >> 9u),
+            qint8(qAlpha(rgba) >> 9u)};
+}
+
+inline constexpr QRgba64 qRgba64(Rgba32Signed rgba)
+{
+    return qRgba64(
+            quint16(std::max<qint8>(0, rgba.red()))   << 9u,
+            quint16(std::max<qint8>(0, rgba.green())) << 9u,
+            quint16(std::max<qint8>(0, rgba.blue()))  << 9u,
+            quint16(std::max<qint8>(0, rgba.alpha())) << 9u);
+}
 
 #endif // RGBA32SIGNED_H
