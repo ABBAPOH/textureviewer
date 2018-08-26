ae4b3c85d44ce2e6f20c6743e731f22a17c640ef
Ivan Komissarov <ABBAPOH@gmail.com>
2018-08-26 13:04:50 +0300

    Add RGBA32_Float support to AnyColor class

--- src/libs/texturelib/anycolor.h
+++ src/libs/texturelib/anycolor.h
@@ -3,6 +3,7 @@
 
 #include <QtGui/QRgb>
 #include <QtGui/QRgba64>
+#include <TextureLib/RgbaFloat32>
 
 class AnyColor
 {
@@ -14,7 +15,7 @@ public:
 //        RGBA16_Float,
 //        RGBA16_Snorm,
         RGBA16_Unorm,
-//        RGBA32_Float,
+        RGBA32_Float,
     };
 
     constexpr AnyColor() noexcept
@@ -29,11 +30,16 @@ public:
         : m_type(Type::RGBA16_Unorm)
         , m_rgba16_Unorm(rgba)
     {}
+    constexpr AnyColor(RgbaFloat32 rgba) noexcept
+        : m_type(Type::RGBA32_Float)
+        , m_rgbaFloat32(rgba)
+    {}
 
     constexpr Type type() const noexcept { return m_type; }
 
     constexpr QRgb toRgba8_Unorm() const noexcept;
     constexpr QRgba64 toRgba16_Unorm() const noexcept;
+    constexpr RgbaFloat32 toRgbaFloat32() const noexcept;
 
 private:
     Type m_type {Type::Invalid};
@@ -44,6 +50,7 @@ private:
         QRgb m_rgba8_Unorm;
 //        quint64 rgba16_snorm;
         QRgba64 m_rgba16_Unorm;
+        RgbaFloat32 m_rgbaFloat32;
     };
 };
 
@@ -53,6 +60,7 @@ constexpr QRgb AnyColor::toRgba8_Unorm() const noexcept
     case Type::Invalid: return {};
     case Type::RGBA8_Unorm: return m_rgba8_Unorm;
     case Type::RGBA16_Unorm: return m_rgba16_Unorm.toArgb32();
+    case Type::RGBA32_Float: return m_rgbaFloat32.toRgba32();
     default: return {};
     }
 }
@@ -63,6 +71,18 @@ constexpr QRgba64 AnyColor::toRgba16_Unorm() const noexcept
     case Type::Invalid: return {};
     case Type::RGBA8_Unorm: return QRgba64::fromArgb32(m_rgba8_Unorm);
     case Type::RGBA16_Unorm: return m_rgba16_Unorm;
+    case Type::RGBA32_Float: return m_rgbaFloat32.toRgba64();
+    default: return {};
+    }
+}
+
+constexpr RgbaFloat32 AnyColor::toRgbaFloat32() const noexcept
+{
+    switch (m_type) {
+    case Type::Invalid: return {};
+    case Type::RGBA8_Unorm: return RgbaFloat32::fromRgba32(m_rgba8_Unorm);
+    case Type::RGBA16_Unorm: return RgbaFloat32::fromRgba64(m_rgba16_Unorm);
+    case Type::RGBA32_Float: return m_rgbaFloat32;
     default: return {};
     }
 }
