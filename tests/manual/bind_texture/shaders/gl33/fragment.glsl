da10efb413d195238be7714ac55c074be4c72245
Ivan Komissarov <ABBAPOH@gmail.com>
2018-08-31 21:38:49 +0200

    Fix gl33/fragment.glsl

--- tests/manual/bind_texture/shaders/gl33/fragment.glsl
+++ tests/manual/bind_texture/shaders/gl33/fragment.glsl
@@ -4,11 +4,9 @@ in vec2 TexCoord;
 
 out vec4 color;
 
-uniform usampler2D ourTexture1;
+uniform sampler2D ourTexture1;
 
 void main()
 {
-    uvec4 tmp = texture(ourTexture1, TexCoord);
-    color = vec4(tmp.x / 127.0f, tmp.y / 255.0f, tmp.z / 255.0f, tmp.w / 255.0f);
-//    color = vec4(1.0f, tmp.g / 255.0f, tmp.b / 255.0f, tmp.a / 255.0f);
+    color = texture(ourTexture1, TexCoord);
 }
