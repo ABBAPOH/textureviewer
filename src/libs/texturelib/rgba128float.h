8938ddba236f8c5f01bf61dd9877bdc561aa737a
Ivan Komissarov <ABBAPOH@gmail.com>
2018-09-30 04:24:55 +0300

    Remove unused methods

--- src/libs/texturelib/rgba128float.h
+++ src/libs/texturelib/rgba128float.h
@@ -10,24 +10,10 @@
 
 using Rgba128Float = RgbaGeneric<float>;
 
-namespace Private {
-
-inline float constexpr normalizeF32(qint8 value) noexcept { return 1.0f * value / 0x7f; }
-inline float constexpr normalizeF32(quint8 value) noexcept { return 1.0f * value / 0xffu; }
-inline float constexpr normalizeF32(quint16 value) noexcept { return 1.0f * value / 0xffffu; }
-inline float constexpr bounded(float value) noexcept { return qBound(0.0f, value, 1.0f); }
-inline float constexpr boundedSigned(float value) noexcept { return qBound(-1.0f, value, 1.0f); }
-
-} // namespace Private
-
-// constructors
-
 inline constexpr Rgba128Float rgba128Float(float r, float g, float b, float a = 1.0f) noexcept
 {
     return Private::createRgba<float>(r, g, b, a);
 }
-
-// Generic
 
 template<typename T>
 inline constexpr Rgba128Float rgba128Float(T rgba) noexcept
--- src/libs/texturelib/rgba64float.h
+++ src/libs/texturelib/rgba64float.h
@@ -9,24 +9,10 @@
 
 using Rgba64Float = RgbaGeneric<HalfFloat>;
 
-namespace Private {
-
-inline HalfFloat constexpr normalizeF16(qint8 value) noexcept { return HalfFloat(1.0f * value / 0x7f); }
-inline HalfFloat constexpr normalizeF16(quint8 value) noexcept { return HalfFloat(1.0f * value / 0xffu); }
-inline HalfFloat constexpr normalizeF16(quint16 value) noexcept { return HalfFloat(1.0f * value / 0xffffu); }
-inline HalfFloat constexpr bounded(HalfFloat value) noexcept { return qBound(HalfFloat(0.0f), value, HalfFloat(1.0f)); }
-inline HalfFloat constexpr boundedSigned(HalfFloat value) noexcept { return qBound(HalfFloat(-1.0f), value, HalfFloat(1.0f)); }
-
-} // namespace Private
-
-// constructors
-
 inline constexpr Rgba64Float rgba64Float(HalfFloat r, HalfFloat g, HalfFloat b, HalfFloat a = HalfFloat(1.0)) noexcept
 {
     return Private::createRgba<HalfFloat>(r, g, b, a);
 }
-
-// Generic
 
 template<typename T>
 inline constexpr Rgba64Float rgba64Float(T rgba) noexcept
