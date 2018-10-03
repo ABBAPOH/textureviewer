9ee2d71a2974595e7221be3ba360d711776bbd3c
Ivan Komissarov <ABBAPOH@gmail.com>
2018-10-03 22:52:12 +0300

    Reorder methods in AnyColor

--- src/libs/texturelib/anycolor.h
+++ src/libs/texturelib/anycolor.h
@@ -3,6 +3,7 @@
 
 #include <TextureLib/Rgba32Signed>
 #include <TextureLib/Rgba64Float>
+#include <TextureLib/Rgba64Signed>
 #include <TextureLib/Rgba128Float>
 
 #include <QtGui/QRgb>
@@ -15,9 +16,9 @@ public:
         Invalid = 0,
         RGBA8_Snorm,
         RGBA8_Unorm,
+        RGBA16_Float,
 //        RGBA16_Snorm,
         RGBA16_Unorm,
-        RGBA16_Float,
         RGBA32_Float,
     };
 
@@ -50,8 +51,8 @@ public:
 
     constexpr Rgba32Signed toRgba8_Snorm() const noexcept;
     constexpr QRgb toRgba8_Unorm() const noexcept;
-    constexpr QRgba64 toRgba16_Unorm() const noexcept;
     constexpr Rgba64Float toRgbaFloat16() const noexcept;
+    constexpr QRgba64 toRgba16_Unorm() const noexcept;
     constexpr Rgba128Float toRgbaFloat32() const noexcept;
 
 private:
@@ -61,9 +62,9 @@ private:
         quint64 m_zero {0};
         Rgba32Signed m_rgba8_Snorm;
         QRgb m_rgba8_Unorm;
+        Rgba64Float m_rgbaFloat16;
 //        quint64 rgba16_snorm;
         QRgba64 m_rgba16_Unorm;
-        Rgba64Float m_rgbaFloat16;
         Rgba128Float m_rgbaFloat32;
     };
 };
@@ -74,8 +75,8 @@ constexpr Rgba32Signed AnyColor::toRgba8_Snorm() const noexcept
     case Type::Invalid: return {};
     case Type::RGBA8_Snorm: return m_rgba8_Snorm;
     case Type::RGBA8_Unorm: return rgba32Signed(m_rgba8_Unorm);
-    case Type::RGBA16_Unorm: return rgba32Signed(m_rgba16_Unorm);
     case Type::RGBA16_Float: return rgba32Signed(m_rgbaFloat16);
+    case Type::RGBA16_Unorm: return rgba32Signed(m_rgba16_Unorm);
     case Type::RGBA32_Float: return rgba32Signed(m_rgbaFloat32);
     default: return {};
     }
@@ -87,8 +88,8 @@ constexpr QRgb AnyColor::toRgba8_Unorm() const noexcept
     case Type::Invalid: return {};
     case Type::RGBA8_Snorm: return qRgba(m_rgba8_Snorm);
     case Type::RGBA8_Unorm: return m_rgba8_Unorm;
-    case Type::RGBA16_Unorm: return m_rgba16_Unorm.toArgb32();
     case Type::RGBA16_Float: return qRgba(m_rgbaFloat16);
+    case Type::RGBA16_Unorm: return m_rgba16_Unorm.toArgb32();
     case Type::RGBA32_Float: return qRgba(m_rgbaFloat32);
     default: return {};
     }
@@ -100,8 +101,8 @@ constexpr QRgba64 AnyColor::toRgba16_Unorm() const noexcept
     case Type::Invalid: return {};
     case Type::RGBA8_Snorm: return qRgba64(m_rgba8_Snorm);
     case Type::RGBA8_Unorm: return QRgba64::fromArgb32(m_rgba8_Unorm);
-    case Type::RGBA16_Unorm: return m_rgba16_Unorm;
     case Type::RGBA16_Float: return qRgba64(m_rgbaFloat16);
+    case Type::RGBA16_Unorm: return m_rgba16_Unorm;
     case Type::RGBA32_Float: return qRgba64(m_rgbaFloat32);
     default: return {};
     }
@@ -113,8 +114,8 @@ constexpr Rgba64Float AnyColor::toRgbaFloat16() const noexcept
     case Type::Invalid: return {};
     case Type::RGBA8_Snorm: return rgba64Float(m_rgba8_Snorm);
     case Type::RGBA8_Unorm: return rgba64Float(m_rgba8_Unorm);
-    case Type::RGBA16_Unorm: return rgba64Float(m_rgba16_Unorm);
     case Type::RGBA16_Float: return m_rgbaFloat16;
+    case Type::RGBA16_Unorm: return rgba64Float(m_rgba16_Unorm);
     case Type::RGBA32_Float: return rgba64Float(m_rgbaFloat32);
     default: return {};
     }
