122d7e4b6895e0831102d757d05d035133ea6cd5
Ivan Komissarov <ABBAPOH@gmail.com>
2018-10-18 22:08:06 +0200

    Add TextureItemModel class.
    This class should replace TextureLayersModel and TextureLevelsModel

---
+++ src/libs/textureviewcore/textureitemmodel.cpp
@@ -0,0 +1,157 @@
+#include "textureitemmodel.h"
+
+namespace TextureViewer {
+
+namespace {
+
+QString getDisplayRole(TextureItemModel::Dimension dim, int row)
+{
+    switch (dim) {
+    case TextureItemModel::Dimension::Face:
+        return TextureItemModel::tr("Face %1").arg(row);
+    case TextureItemModel::Dimension::Level:
+        return TextureItemModel::tr("Level %1").arg(row);
+    case TextureItemModel::Dimension::Layer:
+        return TextureItemModel::tr("Layer %1").arg(row);
+    }
+    Q_UNREACHABLE();
+}
+
+} // namespace
+
+TextureItemModel::TextureItemModel(QObject *parent)
+    : QAbstractListModel(parent)
+{
+}
+
+void TextureItemModel::setDocument(TextureItemModel::TextureDocumentPointer document)
+{
+    if (m_document == document)
+        return;
+
+    if (m_document) {
+        disconnect(m_document.get(), nullptr, this, nullptr);
+    }
+
+    m_document = document;
+    m_face = 0;
+    m_level = 0;
+    m_layer = 0;
+
+    if (m_document) {
+        connect(m_document.get(), &TextureDocument::textureChanged,
+                this, &TextureItemModel::updateItems);
+    }
+
+    updateItems();
+
+    emit documentChanged(m_document);
+    emit faceChanged(m_face);
+    emit levelChanged(m_level);
+    emit layerChanged(m_layer);
+}
+
+void TextureItemModel::setDimension(Dimension dimension)
+{
+    if (m_dimension == dimension)
+        return;
+    m_dimension = dimension;
+    updateItems();
+    emit dimensionChanged(m_dimension);
+}
+
+void TextureItemModel::setFace(int face)
+{
+    if (m_face == face)
+        return;
+    m_face = face;
+    updateItems();
+    emit faceChanged(m_face);
+}
+
+void TextureItemModel::setLevel(int level)
+{
+    if (m_level == level)
+        return;
+    m_level = level;
+    updateItems();
+    emit levelChanged(m_level);
+}
+
+void TextureItemModel::setLayer(int layer)
+{
+    if (m_layer == layer)
+        return;
+    m_layer = layer;
+    updateItems();
+    emit layerChanged(m_layer);
+}
+
+QVariant TextureItemModel::data(const QModelIndex &index, int role) const
+{
+    if (!checkIndex(
+            index,
+            QAbstractItemModel::CheckIndexOption::IndexIsValid
+                    | QAbstractItemModel::CheckIndexOption::ParentIsInvalid)) {
+        qWarning() << "Incorrect index passed to TextureItemModel::data";
+        return QVariant();
+    }
+
+    const auto pos = index.row();
+    if (role == Qt::DisplayRole) {
+        return getDisplayRole(m_dimension, pos);
+    } else if (role == Qt::DecorationRole) {
+        return m_items[size_t(pos)]->thumbnail;
+    }
+
+    return QVariant();
+}
+
+int TextureItemModel::rowCount(const QModelIndex &parent) const
+{
+    if (!checkIndex(parent)) {
+        qWarning() << "Incorrect index passed to TextureItemModel::rowCount";
+        return 0;
+    }
+    return int(m_items.size());
+}
+
+int TextureItemModel::columnCount(const QModelIndex &parent) const
+{
+    if (!checkIndex(parent)) {
+        qWarning() << "Incorrect index passed to TextureItemModel::columnCount";
+        return 0;
+    }
+    return 1;
+}
+
+void TextureItemModel::updateItems()
+{
+    beginResetModel();
+    m_items.clear();
+    if (m_document) {
+        switch (m_dimension) {
+        case Dimension::Face:
+            m_items.reserve(size_t(m_document->faces()));
+            for (int i = 0; i < m_document->faces(); ++i) {
+                m_items.push_back(m_document->item(i, m_level, m_layer));
+            }
+            break;
+        case Dimension::Level:
+            m_items.reserve(size_t(m_document->levels()));
+            for (int i = 0; i < m_document->levels(); ++i) {
+                m_items.push_back(m_document->item(m_face, i, m_layer));
+            }
+            break;
+        case Dimension::Layer:
+            m_items.reserve(size_t(m_document->layers()));
+            for (int i = 0; i < m_document->layers(); ++i) {
+                m_items.push_back(m_document->item(m_face, m_level, i));
+            }
+            break;
+        }
+    }
+    endResetModel();
+}
+
+} // namespace TextureViewer
---
+++ src/libs/textureviewcore/textureitemmodel.h
@@ -0,0 +1,68 @@
+#ifndef TEXTUREITEMMODEL_H
+#define TEXTUREITEMMODEL_H
+
+#include "textureviewcore_global.h"
+
+#include <QtCore/QAbstractListModel>
+
+#include <TextureViewCoreLib/TextureDocument>
+
+namespace TextureViewer {
+
+class TEXTUREVIEWCORE_EXPORT TextureItemModel : public QAbstractListModel
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(TextureItemModel)
+
+    Q_PROPERTY(TextureDocumentPointer document READ document WRITE setDocument NOTIFY documentChanged)
+    Q_PROPERTY(Dimension dimension READ dimension WRITE setDimension NOTIFY dimensionChanged)
+    Q_PROPERTY(int face READ face WRITE setFace NOTIFY faceChanged)
+    Q_PROPERTY(int level READ level WRITE setLevel NOTIFY levelChanged)
+    Q_PROPERTY(int layer READ layer WRITE setLayer NOTIFY layerChanged)
+
+public:
+    using TextureDocumentPointer = ObserverPointer<TextureDocument>;
+    enum class Dimension { Face, Level, Layer };
+
+    explicit TextureItemModel(QObject *parent = nullptr);
+
+    TextureDocumentPointer document() const noexcept { return m_document; }
+    void setDocument(TextureDocumentPointer document);
+    Q_SIGNAL void documentChanged(TextureDocumentPointer document);
+
+    Dimension dimension() const noexcept { return m_dimension; }
+    void setDimension(Dimension dimension);
+    Q_SIGNAL void dimensionChanged(Dimension dimension);
+
+    int face() const noexcept { return m_face; }
+    void setFace(int face);
+    Q_SIGNAL void faceChanged(int face);
+
+    int level() const noexcept { return m_level; }
+    void setLevel(int level);
+    Q_SIGNAL void levelChanged(int layer);
+
+    int layer() const noexcept { return m_layer; }
+    void setLayer(int layer);
+    Q_SIGNAL void layerChanged(int layer);
+
+public: // QAbstractItemModel interface
+    QVariant data(const QModelIndex &index, int role) const override;
+    int rowCount(const QModelIndex &parent) const override;
+    int columnCount(const QModelIndex &parent) const override;
+
+private:
+    void updateItems();
+
+private:
+    TextureDocumentPointer m_document;
+    Dimension m_dimension {Dimension::Face};
+    int m_face {0};
+    int m_level {0};
+    int m_layer {0};
+    std::vector<TextureDocument::ItemPointer> m_items;
+};
+
+} // namespace TextureViewer
+
+#endif // TEXTUREITEMMODEL_H
